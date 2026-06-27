1aac8cea58ed1ee8d86a7dc0ef5ecb3d7ad8ebe7
Ibrahim Adel <ibrahimadel.dev@gmail.com>
2026-06-27 04:40:59 +0300

    cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+
+        $middleware->statefulApi();
+
+        $middleware->encryptCookies(except: [
+            'access_token',
+            'refresh_token',
+        ]);
+        
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
             'auto.refresh' => \App\Http\Middleware\AutoRefreshTokenMiddleware::class,
